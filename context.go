2e2470535e29f7ba53e4760a0b6ebebc5724de80
Liang Liu <liul85@gmail.com>
2020-03-25 21:05:13 +1100

    added JSON, HTML and Data methods in context

--- context.go
+++ context.go
@@ -1,6 +1,7 @@
 package chokos
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
@@ -15,6 +16,30 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 }
 
 func (context *Context) String(statusCode int, content string) {
+	context.writer.Header().Set("Content-Type", "text/plain")
 	context.writer.WriteHeader(statusCode)
 	context.writer.Write([]byte(content))
+}
+
+// JSON method allows user to return json response in response
+func (context *Context) JSON(statusCode int, obj interface{}) {
+	context.writer.Header().Set("Content-Type", "application/json")
+	context.writer.WriteHeader(statusCode)
+	encoder := json.NewEncoder(context.writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(context.writer, err.Error(), 500)
+	}
+}
+
+// HTML method allows user to return html in response
+func (context *Context) HTML(statusCode int, html string) {
+	context.writer.Header().Set("Content-Type", "text/html")
+	context.writer.WriteHeader(statusCode)
+	context.writer.Write([]byte(html))
+}
+
+// Data method allows user to send data in response
+func (context *Context) Data(statusCode int, data []byte) {
+	context.writer.WriteHeader(statusCode)
+	context.writer.Write(data)
 }
